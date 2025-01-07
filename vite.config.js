19b9b460b3cef003da9b1e5eed6e3a2e4733a6a6
prabhu7x <prabhu7x@gmail.com>
2025-01-07 10:02:10 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/product-list-cart-with-main/',
+  base: '/product-list-with-cart-main/',
   plugins: [react()],
 });
 
